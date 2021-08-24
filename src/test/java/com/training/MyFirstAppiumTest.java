bc5fefbef416bddad75592d430bc96036646c153
Amuthan <mech.amuthansakthivel@gmail.com>
2021-08-24 14:01:19 +0530

    Added capability to record tests when ran in local

--- src/test/java/com/training/MyFirstAppiumTest.java
+++ src/test/java/com/training/MyFirstAppiumTest.java
@@ -2,38 +2,37 @@ package com.training;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.AndroidStartScreenRecordingOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.Base64;
 import java.util.List;
 
 public class MyFirstAppiumTest {
 
     //java programs will start from main method
-    public static void main(String[] args) throws MalformedURLException {
+    //video
+    public static void main(String[] args) throws IOException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
-       // capabilities.setCapability("app", System.getProperty("user.dir")+"/ApiDemos-debug.apk");
         capabilities.setCapability("appPackage","io.appium.android.apis");
         capabilities.setCapability("appActivity",".ApiDemos");
         AndroidDriver<AndroidElement> driver =
                 new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
-        //Launch the app in the mobile
 
-        //Locator strategy
-        //driver.findElementByAccessibilityId("Views").click(); //content-desc
-        //driver.findElementByXPath("//android.widget.TextView[@text='Chronometer']").click(); //Xpath
-        //System.out.println(driver.findElementByClassName("android.widget.TextView").getText()); //singular
-        List<AndroidElement> elements = driver.findElementsByClassName("android.widget.TextView");//classname
-        List<AndroidElement> elements1 = driver.findElementsById("android:id/text1"); //id
-        //uiautomator
-        for(int i=0;i< elements1.size();i++){
-            System.out.println(elements1.get(i).getText());
-        }
+        driver.startRecordingScreen(new AndroidStartScreenRecordingOptions().withVideoSize("1280x720")
+                .withTimeLimit(Duration.ofSeconds(200)));
 
-        //long press //swipe //drag and drop
-        //driver.findElementById()
+       driver.findElementByAccessibilityId("Views").click();
+       driver.findElementByAccessibilityId("Drag and Drop").click();
 
+        String videoString = driver.stopRecordingScreen(); //video in base64 encoded string format
+        Files.write(Paths.get(System.getProperty("user.dir")+"/test.mp4"),Base64.getDecoder().decode(videoString));
     }
 
 }
